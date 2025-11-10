e29c42c4b7aea05b121b573e4b63c6919a2a67f6
unknown <biplob.code.bd@gmail.com>
2025-11-10 11:37:33 +0600

    Project Initialization Done

---
+++ src/routes/routes.jsx
@@ -0,0 +1,35 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import NotFound from "../pages/NotFound";
+import Pets from "../pages/Pets";
+import Products from "../pages/Products";
+import PetDetails from "../pages/PetDetails";
+import ProductDetails from "../pages/ProductDetails";
+import Orders from "../pages/Orders";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/signin", element: <SignIn /> },
+      { path: "/signup", element: <SignUp /> },
+      { path: "/pets", element: <Pets /> },
+      { path: "/products", element: <Products /> },
+      { path: "/pet-details", element: <PetDetails /> },
+      { path: "/product-details", element: <ProductDetails /> },
+      { path: "/orders", element: <Orders /> },
+      { path: "/cart", element: <Orders /> },
+    ]
+  },
+  { path: "*", element: <NotFound /> }
+]);
